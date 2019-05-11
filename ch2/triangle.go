207e001e6f9f10dc813874f58adc1e6b4dcbf799
Mavreen Smiel <msmiel@users.noreply.github.com>
2019-05-11 14:36:19 -0400

    final solution

--- ch2/triangle.go
+++ ch2/triangle.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 // // Prints a 5x5 square
 // func main() {
@@ -12,10 +15,20 @@ import "fmt"
 // 	}
 // }
 
-// Prints a half square (base is 5 at top)
+// // Prints a half square (base is 5 at top)
+// func main() {
+// 	for row := 1; row <= 5; row++ {
+// 		for i := 1; i <= 6-row; i++ {
+// 			fmt.Printf("#")
+// 		}
+// 		fmt.Printf("\n")
+// 	}
+// }
+
+// Prints a sideways triangle
 func main() {
-	for row := 1; row <= 5; row++ {
-		for i := 1; i <= 6-row; i++ {
+	for row := 1; row <= 7; row++ {
+		for i := 1; i <= 4-int(math.Abs(float64(4-row))); i++ {
 			fmt.Printf("#")
 		}
 		fmt.Printf("\n")
